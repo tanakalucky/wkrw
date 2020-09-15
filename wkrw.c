375ee8b3046f2860d2f4f027f6bc074e5bae65f5
luckyyeslucky777 <luckyyeslucky777@gmail.com>
2020-09-15 13:51:11 +0900

    feat: "wkrw" move to left to right

--- wkrw.c
+++ wkrw.c
@@ -1,8 +1,38 @@
 # include <stdio.h>
+# include <ncurses.h>
+# include <string.h>
+# include <unistd.h>
 
 int main() 
 {
-  printf("わかるわ\n"); 
+  int x, y, w, h;
+  char *str = "wkrw";
+  int key;
+
+  initscr();
+  noecho();
+  cbreak();
+  timeout(0);
+
+  getmaxyx(stdscr, h, w);
+  y = h/2;
+  x = w;
+  
+  while (1) {
+    erase();
+    move(y, x);
+    addstr(str);
+    refresh();
+
+    key = getch();
+    if (key == 'q') break;
+
+    x--; if (x <= 0) x = w;
+
+    usleep(100000);
+  }
+
+  endwin();
 
   return 0;
 }
