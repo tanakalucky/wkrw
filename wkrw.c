e5aafa32ab611ff0ecbcb98fa79a717f90c52d61
luckyyeslucky777 <luckyyeslucky777@gmail.com>
2020-09-15 16:15:35 +0900

    fix: 画面端で文字列が一瞬ターミナル上端に移動する現象を解消

--- wkrw.c
+++ wkrw.c
@@ -9,14 +9,20 @@ int main()
   char *str = "wkrw";
   int key;
 
+  /* ターミナル初期化 */
   initscr();
+  /* キーボード入力を非表示 */
   noecho();
+  /* enter不要の入力モード */
   cbreak();
+  /* キー入力をまたない */
   timeout(0);
+  /* カーソル非表示 */
+  curs_set(0);
 
   getmaxyx(stdscr, h, w);
   y = h/2;
-  x = w;
+  x = w - strlen(str);
   
   while (1) {
     erase();
@@ -27,7 +33,7 @@ int main()
     key = getch();
     if (key == 'q') break;
 
-    x--; if (x <= 0) x = w;
+    x--; if (x <= 0) x = w - strlen(str);
 
     usleep(100000);
   }
