a940d5dd367d84c31de9bc16a6c26b902732fd07
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2025-09-23 10:36:18 +0900

    Use crossterm for animation rendering

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, thread, time::Duration};
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::execute;
+use crossterm::terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen};
+use std::{io::stdout, io::Result, io::Write, thread, time::Duration};
 
 const WKRW: [&str; 15] = [
     r#"    #                 #                                #            "#,
@@ -20,20 +23,18 @@ const WKRW: [&str; 15] = [
 
 const FRAME_DELAY: u64 = 40000;
 
-fn main() {
-    // ターミナルのサイズを取得
+fn main() -> Result<()> {
+    let mut stdout = stdout();
+
     let screen_width = term_size::dimensions().map(|(w, _)| w).unwrap_or(80); // 取得できない場合は80を使用
 
-    // カーソル位置を保存
-    print!("\x1B[?47h"); // 代替画面バッファに切り替え
-    print!("\x1B[?25l"); // カーソルを非表示
+    execute!(stdout, EnterAlternateScreen, Hide)?;
 
     let text_width = WKRW[0].len();
 
     // 文字列が完全に表示されてから消えるまでを表現
     for x in (-(text_width as i32)..screen_width as i32).rev() {
-        // 画面をクリアしてカーソルを先頭に移動
-        print!("\x1B[2J\x1B[1;1H");
+        execute!(stdout, Clear(ClearType::All), MoveTo(0, 0))?;
 
         // 各行を処理
         for line in WKRW.iter() {
@@ -58,15 +59,14 @@ fn main() {
         }
 
         // 出力をフラッシュして確実に表示
-        Write::flush(&mut std::io::stdout()).unwrap();
+        Write::flush(&mut stdout)?;
 
         thread::sleep(Duration::from_micros(FRAME_DELAY));
     }
 
-    // 画面をクリアし、カーソルを元の位置に戻す
-    print!("\x1B[2J"); // 画面クリア
-    print!("\x1B[?47l"); // メイン画面バッファに戻る
-    print!("\x1B[?25h"); // カーソルを再表示
-    println!(); // 新しい行に移動
-    Write::flush(&mut std::io::stdout()).unwrap();
+    execute!(stdout, Hide, LeaveAlternateScreen, Show)?;
+
+    Write::flush(&mut stdout)?;
+
+    Ok(())
 }
